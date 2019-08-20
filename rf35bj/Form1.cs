10662843d4107fc0294705dad30c4ed38f04e81b
gitjfl <279168581@qq.com>
2019-08-20 16:08:22 +0800

    first commit

---
+++ rf35bj/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rf35bj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            byte[] by = new byte[] { };
+        }
+
+        private string StringToHexString(string s, Encoding encode)
+        {
+            byte[] b = encode.GetBytes(s);//按照指定编码将string编程字节数组
+            string result = string.Empty;
+            for (int i = 0; i < b.Length; i++)//逐字节变为16进制字符，以%隔开
+            {
+                result += "%" + Convert.ToString(b[i], 16);
+            }
+            return result;
+        }
+
+
+        public int icdev;
+        int st;
+        byte[] snr = new byte[5];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            icdev = Program.rf_usbopen();
+            if (icdev > 0)
+            {
+                listBox1.Items.Add("Com Connect success!");
+                byte[] status = new byte[30];
+                st = Program.rf_get_status(icdev, status);
+                //lbHardVer.Text=System.Text.Encoding.ASCII.GetString(status);
+                listBox1.Items.Add(System.Text.Encoding.Default.GetString(status));
+                Program.rf_beep(icdev, 25);
+            }
+            else
+                listBox1.Items.Add("Com Connect failed!");
+
+            byte[] key = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+            int mode = 0;
+            int sector = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                st = Program.rf_load_key(icdev, mode, sector, key);
+                if (st != 0)
+                {
+                    string s1 = Convert.ToString(sector);
+                    listBox1.Items.Add(s1 + " sector rf_load_key error!");
+                }
+                sector++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sector = 0;
+            st = Program.rf_card(icdev, 1, snr);
+            if (st != 0)
+            {
+                listBox1.Items.Add("rf_card error");
+            }
+            else
+            {
+                byte[] snr1 = new byte[8];
+                string snvalue = string.Empty;
+                listBox1.Items.Add("rf_card right!");
+                Program.hex_a(snr, snr1, 4);
+                snvalue = System.Text.Encoding.Default.GetString(snr1);
+                listBox1.Items.Add(snvalue);
+                listBox1.Items.Add(Convert.ToInt64(snvalue, 16));
+            }
+            st = Program.rf_authentication(icdev, 0, sector);
+            if (st != 0)
+            {
+                listBox1.Items.Add("rf_authentication error!");
+            }
+            else
+            {
+                listBox1.Items.Add("rf_authentication right!");
+            }
+
+            byte[] data = new byte[16];
+            string databuff = "123456";
+            data = Encoding.Default.GetBytes(databuff);
+            //st = Program.rf_write(icdev, sector * 4 + 1, data);
+            //if (st != 0)
+            //{
+            //    listBox1.Items.Add("rf_write error!");
+            //    listBox1.Items.Add(st.ToString());
+            //}
+            //else
+            //{
+            //    listBox1.Items.Add("rf_write right!");
+            //}
+
+            byte[] databuffer = new byte[16];
+
+            st = Program.rf_read(icdev, sector * 4 + 0, databuffer);
+            string s = string.Empty;
+            for (int i = 0; i < 16; i++)
+            {
+                s += Convert.ToString(databuffer[i], 16);
+            }
+            listBox1.Items.Add(s);
+            if (st != 0)
+            {
+                listBox1.Items.Add("rf_read error!");
+                listBox1.Items.Add(st.ToString());
+            }
+            else
+            {
+                listBox1.Items.Add("rf_read right!");
+                listBox1.Items.Add(System.Text.Encoding.Default.GetString(databuffer));
+            }
+
+            byte[] keya = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+            byte[] keyb = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+            st = Program.rf_changeb3(icdev, sector, keya, 0, 0, 0, 1, 0, keyb);
+            if (st != 0)
+            {
+                listBox1.Items.Add("rf_changeb3 error!");
+                listBox1.Items.Add(st.ToString());
+            }
+            else
+            {
+                listBox1.Items.Add("rf_changeb3 right!");
+            }
+            Program.rf_beep(icdev, 50);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            st = Program.rf_usbclose(icdev);
+            if (st == 0)
+            {
+                listBox1.Items.Add("断开连接！");
+            }
+        }
+    }
+}
---
+++ rf35bj/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace rf35bj
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_usbopen", SetLastError = true,
+               CharSet = CharSet.Auto, ExactSpelling = false,
+               CallingConvention = CallingConvention.StdCall)]
+        public static extern int rf_usbopen();
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_usbclose", SetLastError = true,
+               CharSet = CharSet.Auto, ExactSpelling = false,
+               CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_usbclose(int icdev);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_beep", SetLastError = true,
+               CharSet = CharSet.Auto, ExactSpelling = false,
+               CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_beep(int icdev, int msec);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_get_status", SetLastError = true,
+              CharSet = CharSet.Auto, ExactSpelling = false,
+              CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_get_status(int icdev, [MarshalAs(UnmanagedType.LPArray)]byte[] state);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_load_key", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_load_key(int icdev, int mode, int secnr, [MarshalAs(UnmanagedType.LPArray)]byte[] keybuff);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_load_key_hex", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_load_key_hex(int icdev, int mode, int secnr, [MarshalAs(UnmanagedType.LPArray)]byte[] keybuff);
+
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "a_hex", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 a_hex([MarshalAs(UnmanagedType.LPArray)]byte[] asc, [MarshalAs(UnmanagedType.LPArray)]byte[] hex, int len);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "hex_a", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 hex_a([MarshalAs(UnmanagedType.LPArray)]byte[] hex, [MarshalAs(UnmanagedType.LPArray)]byte[] asc, int len);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_reset", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_reset(int icdev, int msec);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_request", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_request(int icdev, int mode, out UInt16 tagtype);
+
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_anticoll", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_anticoll(int icdev, int bcnt, out uint snr);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_select", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_select(int icdev, uint snr, out byte size);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_card", SetLastError = true,
+            CharSet = CharSet.Auto, ExactSpelling = false,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_card(int icdev, int mode, [MarshalAs(UnmanagedType.LPArray)]byte[] snr);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_authentication", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_authentication(int icdev, int mode, int secnr);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_authentication_2", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_authentication_2(int icdev, int mode, int keynr, int blocknr);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_read", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_read(int icdev, int blocknr, [MarshalAs(UnmanagedType.LPArray)]byte[] databuff);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_read_hex", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_read_hex(int icdev, int blocknr, [MarshalAs(UnmanagedType.LPArray)]byte[] databuff);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_write_hex", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_write_hex(int icdev, int blocknr, [MarshalAs(UnmanagedType.LPArray)]byte[] databuff);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_write", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_write(int icdev, int blocknr, [MarshalAs(UnmanagedType.LPArray)]byte[] databuff);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_halt", SetLastError = true,
+             CharSet = CharSet.Auto, ExactSpelling = false,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_halt(int icdev);
+
+        [DllImport("mwhrf_bj.dll", EntryPoint = "rf_changeb3", SetLastError = true,
+            CharSet = CharSet.Auto, ExactSpelling = false,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_changeb3(int icdev, int sector, [MarshalAs(UnmanagedType.LPArray)]byte[] keya, int B0, int B1,
+              int B2, int B3, int Bk, [MarshalAs(UnmanagedType.LPArray)]byte[] keyb);
+    }
+}
